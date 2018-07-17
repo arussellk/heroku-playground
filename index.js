d33518edbb367bc5890b54beabf6cd9533b41ac8
Russell Kennington <arussellk@gmail.com>
2018-07-16 19:43:22 -0600

    Change PORT

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const PORT = process.env.PORT || 5000
 
 express()
   .get('/', (req, res) => res.json({ hello: 'world' }))
-  .listen(8080, () => console.log('Listening on port 8080.'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
